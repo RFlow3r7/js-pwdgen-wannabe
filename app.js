26a8e039e33299a1f0addabc10c7e9cf325cf527
RFlow3r7 <andrefiore19967@gmail.com>
2024-03-06 20:08:25 +0100

    Corretto stringa 10

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ let colorePreferito = prompt("Digita il tuo colore preferito:");
 
 let numeroCasuale = Math.floor(Math.random() * 100);
 
-let password = nome + cognome + colorePreferito + eta + numeroCasuale;
+let password = nome + cognome + colorePreferito + numeroCasuale;
 
 console.log("La tua password generata è:", password);
