62fadfb963186ec75dd8c78340c07729c026bf2e
RFlow3r7 <andrefiore19967@gmail.com>
2024-03-06 17:24:34 +0100

    Esercizio Completo

---
+++ app.js
@@ -0,0 +1,12 @@
+
+let nome = prompt("Digita il tuo nome:");
+
+let cognome = prompt("Digita il tuo cognome:");
+
+let colorePreferito = prompt("Digita il tuo colore preferito:");
+
+let numeroCasuale = Math.floor(Math.random() * 100);
+
+let password = nome + cognome + colorePreferito + eta + numeroCasuale;
+
+console.log("La tua password generata è:", password);
